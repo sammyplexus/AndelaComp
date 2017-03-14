cf96b623485ea6ebfe69ea2aedb3e6b09d5734f5
Agbede Samuel D <Agbede Samuel D>
2017-03-15 00:59:44 +0100

    Added custom filter functionality and implemented holder pattern for views

--- app/src/main/java/com/samuelagbede/freelance/andelacomp/Adapter/ListAdapter.java
+++ app/src/main/java/com/samuelagbede/freelance/andelacomp/Adapter/ListAdapter.java
@@ -8,10 +8,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.samuelagbede.freelance.andelacomp.Activity.SplashActivity;
 import com.samuelagbede.freelance.andelacomp.Model.UsersModel;
 import com.samuelagbede.freelance.andelacomp.R;
 import com.squareup.picasso.Callback;
@@ -26,46 +28,73 @@ import de.hdodenhof.circleimageview.CircleImageView;
  * Created by Agbede Samuel D on 3/6/2017.
  */
 
-public class ListAdapter extends ArrayAdapter {
+public class ListAdapter extends ArrayAdapter<UsersModel> implements Filterable {
     Context context;
     ArrayList<UsersModel> usersModels;
+    ArrayList<UsersModel> originalUsersModels;
     LayoutInflater layoutInflater;
+    AdapterFilter filter;
+
 
     public ListAdapter(Context context, int resource, ArrayList<UsersModel> usersModels) {
         super(context, resource);
         this.usersModels = usersModels;
         this.context = context;
+        this.originalUsersModels = usersModels;
     }
-
 
     @Override
     public int getCount() {
         return usersModels.size();
     }
 
+    @Nullable
+    @Override
+    public UsersModel getItem(int position) {
+        return usersModels.get(position);
+    }
 
+    @Override
+    public long getItemId(int position) {
+        return usersModels.get(position).hashCode();
+    }
 
     @NonNull
     @Override
     public View getView(final int position, View convertView, ViewGroup parent) {
-        if (convertView == null){
+        View v = convertView;
+
+        Holder viewHolder = new Holder();
+
+        if (convertView == null)
+        {
             layoutInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            convertView = layoutInflater.inflate(R.layout.individual_list, null, true);
+            v = layoutInflater.inflate(R.layout.individual_list, null, true);
+
+            CircleImageView imageView = (CircleImageView)v.findViewById(R.id.user_image);
+            TextView userName = (TextView) v.findViewById(R.id.user_name);
+
+            viewHolder.imageView = imageView;
+            viewHolder.textView = userName;
+
+            v.setTag(viewHolder);
+        }
+        else
+        {
+            viewHolder = (Holder)v.getTag();
         }
 
-        TextView userName = (TextView) convertView.findViewById(R.id.user_name);
-        final CircleImageView imageView = (CircleImageView)convertView.findViewById(R.id.user_image);
-        userName.setText(usersModels.get(position).getUsername());
-
-        Picasso.with(context).load(usersModels.get(position).getImage_url()).networkPolicy(NetworkPolicy.OFFLINE).placeholder(R.mipmap.ic_launcher).fit().into(imageView, new Callback() {
+        viewHolder.textView.setText(usersModels.get(position).getUsername());
+        final Holder finalViewHolder = viewHolder;
+        Picasso.with(context).load(usersModels.get(position).getImage_url()).networkPolicy(NetworkPolicy.OFFLINE).placeholder(R.mipmap.ic_launcher).fit().into(viewHolder.imageView, new Callback() {
             @Override
             public void onSuccess() {
-
+                Log.d("Running from cache", "Picasso is great");
             }
 
             @Override
             public void onError() {
-                Picasso.with(context).load(usersModels.get(position).getImage_url()).placeholder(R.mipmap.ic_launcher).fit().into(imageView, new Callback()
+                Picasso.with(context).load(usersModels.get(position).getImage_url()).placeholder(R.mipmap.ic_launcher).fit().into(finalViewHolder.imageView, new Callback()
                 {
                     @Override
                     public void onSuccess()
@@ -82,6 +111,70 @@ public class ListAdapter extends ArrayAdapter {
             }
         });
 
-        return convertView;
+        return v;
     }
+
+    public void resetData(){
+        usersModels = originalUsersModels;
+    }
+
+    @Override
+    public Filter getFilter() {
+        if (filter == null)
+        filter = new AdapterFilter();
+        return filter;
+    }
+
+
+    private static class Holder
+    {
+        CircleImageView imageView;
+        TextView textView;
+    }
+
+
+    private class AdapterFilter extends Filter
+    {
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint)
+        {
+            FilterResults filterResults = new FilterResults();
+            if (constraint == null || constraint.length() == 0)
+            {
+                filterResults.count = originalUsersModels.size();
+                filterResults.values = originalUsersModels;
+            }
+
+            else {
+                ArrayList<UsersModel> mUsersModels = new ArrayList<>();
+
+                for (UsersModel model : usersModels)
+                {
+                    if (model.getUsername().toLowerCase().contains(constraint.toString().toLowerCase()))
+                    {
+                        mUsersModels.add(model);
+                    }
+                }
+
+                filterResults.count = mUsersModels.size();
+                filterResults.values = mUsersModels;
+
+            }
+            return filterResults;
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            if (results.count == 0){
+                notifyDataSetInvalidated();
+            }
+            else {
+                usersModels = (ArrayList<UsersModel>) results.values;
+                notifyDataSetChanged();
+            }
+
+        }
+    }
+
+
 }
