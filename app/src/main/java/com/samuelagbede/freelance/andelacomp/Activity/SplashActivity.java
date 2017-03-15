87f3a9cb251f1abb0cb8148ef8623e23e02c3296
Agbede Samuel D <Agbede Samuel D>
2017-03-15 21:44:51 +0100

    Added retry button and network check

--- app/src/main/java/com/samuelagbede/freelance/andelacomp/Activity/SplashActivity.java
+++ app/src/main/java/com/samuelagbede/freelance/andelacomp/Activity/SplashActivity.java
@@ -1,9 +1,17 @@
 package com.samuelagbede.freelance.andelacomp.Activity;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.TranslateAnimation;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -20,6 +28,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 /**
@@ -27,12 +36,16 @@ import java.util.ArrayList;
  */
 
 public class SplashActivity extends Activity {
-    private Indicator indicator;
+    private View indicator;
     private Singleton singleton;
     private String url_string ="https://api.github.com/search/users?q=location:lagos+language:java&per_page=100";
     private RequestQueue requestQueue;
     private final String TAG = "request";
     private ArrayList<UsersModel> usersModelArrayList;
+    private TextView warning;
+    private TextView aww_snap;
+    private TextView loading_text;
+    private Button retry;
     UsersModel usersModel;
     JSONArray jsonArray;
 
@@ -40,12 +53,73 @@ public class SplashActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        indicator = findViewById(R.id.avi);
+        warning = (TextView) findViewById(R.id.warning);
+        aww_snap = (TextView) findViewById(R.id.snap);
+        loading_text = (TextView) findViewById(R.id.loading) ;
+        retry = (Button) findViewById(R.id.retry);
 
 
         usersModelArrayList = new ArrayList<>();
         singleton = Singleton.getInstance(this);
 
 
+        if(!isNetworkAvailable() || !isInternetAvailable())
+        {
+            showNeedful();
+        }
+        else
+        {
+            getData();
+        }
+
+
+        retry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onRetryClick();
+                getData();
+            }
+        });
+
+
+
+
+    }
+    private  void onRetryClick(){
+        loading_text.setVisibility(View.VISIBLE);
+        indicator.setVisibility(View.VISIBLE);
+        warning.setVisibility(View.GONE);
+        aww_snap.setVisibility(View.GONE);
+        retry.setVisibility(View.GONE);
+    }
+
+    private void showNeedful(){
+        loading_text.setVisibility(View.GONE);
+        indicator.setVisibility(View.GONE);
+        warning.setVisibility(View.VISIBLE);
+        aww_snap.setVisibility(View.VISIBLE);
+        retry.setVisibility(View.VISIBLE);
+    }
+
+    boolean isInternetAvailable(){
+        Runtime runtime = Runtime.getRuntime();
+        try {
+            Process ipProcess = runtime.exec("/system/bin/ping -c 1 8.8.8.8");
+            int     exitValue = ipProcess.waitFor();
+            return (exitValue == 0);
+        } catch (IOException | InterruptedException e)          { e.printStackTrace(); }
+        return false;
+    }
+
+    private Boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnectedOrConnecting();
+    }
+
+    private void getData(){
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url_string, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject jsonObject)
@@ -78,14 +152,12 @@ public class SplashActivity extends Activity {
             @Override
             public void onErrorResponse(VolleyError volleyError)
             {
-                Toast.makeText(SplashActivity.this, "Network is probably poor. Check your connection and try again", Toast.LENGTH_LONG).show();
+                showNeedful();
             }
 
         });
         jsonObjectRequest.setTag(TAG);
         singleton.getRequestQueue().add(jsonObjectRequest);
-
-
     }
 
     @Override
